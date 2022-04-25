975c78850624c3192f6b64b7cebb78648913df87
Andrew Osborn <osbornandrewj@gmail.com>
2022-04-25 13:13:46 -0700

    commit tree 927bf4df5f7e409c30cbc38ae1efef5c6dc519df

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,8 +2,16 @@ import React from "react";
 
 function NavBar() {
   const links = ["home", "about", "projects"];
-
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return <nav>{
+    <li>
+      <a key="1" href="#home">home</a> 
+      <a key="2" href="#about">about</a>
+      <a key="3" href="#projects">projects</a>
+    </li>
+    }</nav>;
 }
 
 export default NavBar;
+// const linkElements = links.map((link) => {
+//   return <a key={link} href="#home">{link}</a>
+// })
